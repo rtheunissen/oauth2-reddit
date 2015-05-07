5428963cde5418b6e057208e1da79261a530ba23
Rudi Theunissen <rudolf.theunissen@gmail.com>
2015-05-07 13:55:03 +1200

    Fixed provider namespace

--- src/Provider/Reddit.php
+++ src/Provider/Reddit.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Rudolf\OAuth2\Client\Reddit\Provider;
+namespace Rudolf\OAuth2\Client\Provider;
 
 use League\OAuth2\Client\Provider\AbstractProvider;
 use League\OAuth2\Client\Token\AccessToken;
